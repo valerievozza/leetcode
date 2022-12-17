17676f347e63266ebae6587ff20fb5b9eba856bd
valerievozza <valerievozza@gmail.com>
2022-12-17 10:38:47 -0800

    add solution without method

--- 0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -22,9 +22,73 @@
  * @param {string} needle
  * @return {number}
  */
-var strStr = function(haystack, needle) {
-    return haystack.indexOf(needle)
-};
+// var strStr = function(haystack, needle) {
+//   // if there are no chars in needle or needle is the same as haystack, return 0
+//   if (needle === '' || needle === haystack) return 0
+//   // if needle is longer than haystack return -1
+//   if (needle.length > haystack.length) return -1
+
+//   // loop through haystack until the remaining substring is shorter than needle
+//   for (let i = 0; i < haystack.length - needle.length + 1; i++) {
+//     // if current char of haystack matches the first char of needle
+//     if (haystack[i] === needle[0]) {
+//       // loop through needle to check that chars match chars in haystack starting at h[i]
+//       for (let j = 0; j < needle.length; j++) {
+//         // if they don't match break out of the loop and return to haystack loop
+//         if (needle[j] !== haystack[i+j]) {
+//           break;
+//         // if we reach the end of needle and all the chars have matched return the index of the char we started the loop at
+//         } else if (j === needle.length - 1) {
+//           return i
+//         }
+//       }
+//     }
+//   }
+//   return -1
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function strStr(haystack, needle) {
+  if (needle === '' || needle === haystack) return 0
+  if (needle.length > haystack.length) return -1
+
+  for (let i = 0; i < haystack.length - needle.length + 1; i++) {
+    if (haystack[i] === needle[0]) {
+      for (let j = 0; j < needle.length; j++) {
+        if (needle[j] !== haystack[i+j]) {
+          break;
+        } else if (j === needle.length - 1) {
+          return i
+        }
+      }
+    }
+  }
+  return -1
+}
 
 console.log(strStr("sadbutsad", "sad"), 0)
 console.log(strStr("leetcode", "leeto"), -1)
